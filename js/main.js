221c90a95f596c65cf2b3094f2c8ee59a0b53044
Felipe Melo <melolemoslima@gmail.com>
2025-05-04 18:13:23 -0300

    correção

--- js/main.js
+++ js/main.js
@@ -1,28 +1,46 @@
-$('form').validate({
-    rules: {
-        nome: { required: true },
-        email: { required: true, email: true },
-        telefone: { required: true },
-        endereco: { required: true },
-        cep: { required: true },
-        cpf: { required: true }
-    },
-    messages: {
-        nome: { required: "Por favor, informe o seu nome." },
-        email: {
-            required: "O e-mail é obrigatório.",
-            email: "Por favor, insira um e-mail válido."
+$(document).ready(function () {
+    // Máscaras
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(DDD) 12345-6789'
+    });
+
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
+    });
+
+    $('#cep').mask('00000-000', {
+        placeholder: '01234-567'
+    });
+
+    // Validação
+    $('#form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            }
         },
-        telefone: { required: "Por favor, informe seu telefone." },
-        endereco: { required: "O endereço é obrigatório." },
-        cep: { required: "O CEP é obrigatório." },
-        cpf: { required: "O CPF é obrigatório." }
-    },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
-    },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
+        submitHandler: function (form) {
+            alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+            form.reset();
+        },
+        invalidHandler: function () {
+            alert("Por favor, preencha os campos para prosseguir com a compra!");
+        }
+    });
 });
